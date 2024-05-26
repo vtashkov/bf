ae2d71d19bf1ddf6fbed0c65b385414b702c9b47
Victor Tashkov <vtashkov@gmail.com>
2024-05-26 18:42:03 +0300

    reading input file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{env, fs, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("bf: error: no input file");
+        process::exit(1);
+    }
+
+    let file_path = &args[1];
+
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("{contents}");
 }
