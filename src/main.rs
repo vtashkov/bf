43f7edda85054957ba11a66aba8b82141ab7def4
Victor Tashkov <vtashkov@gmail.com>
2024-05-27 13:20:59 +0300

    small refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,27 @@
 use std::{env, fs, io::ErrorKind, process};
 
 fn main() {
+    let input_file_path = parse_cmd_arguments().unwrap_or_else(error_and_abort());
     
-    let file_path = parse_cmd_arguments().unwrap_or_else(error_and_abort());
-
-    let contents = read_file(&file_path).unwrap_or_else(error_and_abort());
+    let contents = read_file_contents(&input_file_path).unwrap_or_else(error_and_abort());
 
     println!("{contents}");
 }
 
 fn parse_cmd_arguments() -> Result<String, String> {
     let args: Vec<String> = env::args().collect();
-
     if args.len() < 2 {
         return Err(String::from("no input file"));
     }
-
-    let file_path = args[1].clone();
-
-    Ok(file_path)
+    let input_file_path = args[1].clone();
+    Ok(input_file_path)
 }
 
-fn read_file(file_path: &str) -> Result<String, String> {
-    let contents_result = fs::read_to_string(file_path);
-
-    let contents = match contents_result {
-        Ok(contents) => contents,
-        Err(error) => { 
-            match error.kind() {
-                ErrorKind::NotFound => return Err(format!("no such file: '{file_path}'")),
-                _ => return Err(error.to_string())
-            }
-        }
-    };
-
-    Ok(contents)
+fn read_file_contents(input_file_path: &str) -> Result<String, String> {
+    fs::read_to_string(input_file_path).map_err(|error| match error.kind() {
+        ErrorKind::NotFound => format!("no such file: '{input_file_path}'"),
+        _ => error.to_string()
+    })
 }
 
 fn error_and_abort() -> impl FnOnce(String) -> String {
